bf33155772dd3f35b1df98183411131f8c7e1c29
Maharishi R <rmaharishir@gmail.com>
2019-11-16 19:48:54 -0800

    commit on 'master'

--- mail.php
+++ mail.php
@@ -3,7 +3,6 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $call = $_POST['call'];
-$website = $_POST['website'];
 $priority = $_POST['priority'];
 $type = $_POST['type'];
 $message = $_POST['message'];
