c411b5d9963a74ec0d6dbbb761ff4109ec46836e
Maharishi R <rmaharishir@gmail.com>
2019-11-16 20:12:27 -0800

    commit on 'master'

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function() {
     $(document).on("mousewheel", function() {
-      if($(document).scrollBottom() > 100){
+      if($(document).scrollBottom() > 50){
           $('footer').hide();
       } else {
           $('footer').show();
       }; 
-    });
+    })
   });
