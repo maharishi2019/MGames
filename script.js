78b3abd0c7b36027eb53bdbd692edd88bdd3ad02
Maharishi R <rmaharishir@gmail.com>
2019-11-17 14:54:23 -0800

    commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var userClickedHideFooter = 0; 
 function hideFooter(){
     document.getElementById("webfooter").style.display= 'none'; 
+    userClickedHideFooter = 1; 
+}
+
+if (userClickedHideFooter === 1){
+    document.getElementById("webfooter").style.display='none'; 
 }
